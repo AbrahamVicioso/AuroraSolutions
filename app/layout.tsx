a33bb46413fd2a67b66410c3003916309cfc99a7
Abraham Vicioso <abrahanvicioso@gmail.com>
2025-03-06 22:33:48 -0400

    add: Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Aurora Solutions - Soluciones inteligentes para empresas",
   description:
     "Gestiona tus ventas, contabilidad, facturación y nómina de forma automática en un solo lugar, fácil y rápido.",
-    generator: 'v0.dev'
+    generator: 'v0.dev',
 }
 
 export default function RootLayout({
